148951bcf5777e03c8ddafc443f175792154bdfa
BDShanks28 <42577330+BDShanks28@users.noreply.github.com>
2022-11-22 13:55:54 -0600

    Adding the primary schema code

---
+++ Movie_Rental_Schema_Maker.sql
@@ -0,0 +1,29 @@
+CREATE SCHEMA Movie_Rental;
+
+CREATE TABLE Movie_Rental.customers(
+	customer_ID varchar(11),
+    address varchar(11),
+    fname varchar(11),
+    mname varchar(11),
+    lname varchar(11)
+)
+
+CREATE TABLE Movie_Rental.movie(
+		
+)
+
+CREATE TABLE Movie_Rental.inventory(
+	
+)
+
+CREATE TABLE Movie_Rental.store(
+	
+)
+
+CREATE TABLE Movie_Rental.rent(
+	
+)
+
+CREATE TABLE Movie_Rental.payment(
+	
+)

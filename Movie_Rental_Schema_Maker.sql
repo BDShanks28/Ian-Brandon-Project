6c5f780dcd1efb9c7f57c9fc02db7cb6db442878
BDShanks28 <bshankle28@gmail.com>
2022-11-23 16:06:58 -0600

    Small edits
    Will add foreign keys next

--- Movie_Rental_Schema_Maker.sql
+++ Movie_Rental_Schema_Maker.sql
@@ -5,7 +5,8 @@ CREATE TABLE Movie_Rental.customers(
     address varchar(11),
     fname varchar(11),
     minit varchar(11),
-    lname varchar(11)
+    lname varchar(11),
+    Primary key (customer_ID)
 );
 
 CREATE TABLE Movie_Rental.movie(
@@ -14,31 +15,37 @@ CREATE TABLE Movie_Rental.movie(
 	movie_Genre varchar(11),
 	movie_Rating varchar(11),
     movie_Director varchar(11),
-    movie_Actors varchar(11)
+    movie_Actors varchar(11),
+    primary key (movie_ID)
 );
 
 CREATE TABLE Movie_Rental.inventory(
 	num_of_Movies int,
     m_ID int,
-    inventory_Locations varchar(11)
+    inventory_Locations varchar(11),
+    primary key (num_of_Movies)
 );
 
 CREATE TABLE Movie_Rental.store(
 	store_ID int,
     store_Location varchar(11),
-    inventory_Locations varchar(11)
+    inventory_Locations varchar(11),
+    primary key (store_ID)
 );
 
 CREATE TABLE Movie_Rental.rent(
 	rent_ID int,
     start_Date date,
     end_Date date,
-    overdue double
+    overdue double,
+    primary key (rent_ID)
 );
 
 CREATE TABLE Movie_Rental.payment(
-	paid double,
+	payment_ID int,
+    paid double,
     payment_Method varchar(11),
     startDate date,
-    endDate date
+    endDate date,
+    primary key (payment_ID)
 );
